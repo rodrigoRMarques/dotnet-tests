f93220d50d7cf0d1ef1f171b015c04fc7c7e898b
Rodrigo Marques <roroma@onda.com.br>
2023-06-02 09:54:21 -0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+
+public class Program
+{
+	public static void Main()
+	{
+		Incremental();
+        Delegate();
+	}
+	
+	public static void Incremental()
+	{
+        Console.WriteLine("Incremental");
+		for(int i = 0; i < 10; i++)
+		{
+			//Console.WriteLine(i);
+		}
+				
+		int number = 0;
+		Console.WriteLine(number);
+		Console.WriteLine(number++);
+		Console.WriteLine(++number);
+	}
+	
+	public delegate void Printer();
+	public static void Delegate()
+	{
+        Console.WriteLine("Delegate");
+		List<Printer> printers = new List<Printer>();
+		
+		for(int i=0; i<10; i++){
+			printers.Add(delegate {Console.WriteLine(i); });
+		}
+
+		foreach (var printer in printers){
+			printer();
+		}
+	}
+}
