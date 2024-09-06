9258183f8f209e0721325c9d03eac36b86512b6e
Rodrigo Marques <rodrigo.rmarques@outlook.com>
2024-09-06 15:46:01 -0300

    indentation

--- Program.cs
+++ Program.cs
@@ -3,34 +3,36 @@ using System.Collections.Generic;
 
 public class Program
 {
-	public static void Main()
-	{
-		Incremental();
+    public static void Main()
+    {
+        Incremental();
         Delegate();
-	}
-	
-	public static void Incremental()
-	{
-        Console.WriteLine("Incremental");
-				
-		int number = 0;
-		Console.WriteLine(number);
-		Console.WriteLine(number++);
-		Console.WriteLine(++number);
-	}
-	
-	public delegate void Printer();
-	public static void Delegate()
-	{
-        Console.WriteLine("Delegate");
-		List<Printer> printers = new List<Printer>();
-		
-		for(int i=0; i<10; i++){
-			printers.Add(delegate {Console.WriteLine(i); });
-		}
+    }
 
-		foreach (var printer in printers){
-			printer();
-		}
-	}
+    public static void Incremental()
+    {
+        Console.WriteLine("Incremental");
+
+        int number = 0;
+        Console.WriteLine(number);
+        Console.WriteLine(number++);
+        Console.WriteLine(++number);
+    }
+
+    public delegate void Printer();
+    public static void Delegate()
+    {
+        Console.WriteLine("Delegate");
+        List<Printer> printers = new List<Printer>();
+
+        for (int i = 0; i < 10; i++)
+        {
+            printers.Add(delegate { Console.WriteLine(i); });
+        }
+
+        foreach (var printer in printers)
+        {
+            printer();
+        }
+    }
 }
